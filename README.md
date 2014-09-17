587d4329b4a0fffd5459ff65f5db52fe0fd338c2
BryanChik <bryan.yychik@gmail.com>
2014-09-17 22:57:22 +0800

    second commitment, project finished!! date 17/9/2014

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-datasciencecoursera
-===================
+#Readme file for Project
+
+##Purpose
+This package contains a script to perform data cleaning and extraction from the UCI HAR Dataset. This script will first merge the test set, training set, subjects in the test set, subjects in the training set, activities in the test set amd activity in the training set into one data set, and then extract the mean and standard deviation columns from the original dataset (feature variable names containing "mean()"" and "std()"). Then the script will name the variable with descriptive names. Finally a second independent data set containing the mean values of each feature variable for each subject and each activity will be outputted. 
+
+##File Package
+The file package contain 3 files:
+1.run_analysis.R -- Main script for performing the cleaning and extraction action
+2.README.md -- A README markdown file containing instructions to run the script as well as purpose
+3.CODEBOOK.md -- Codebook containing description of the variables
+
+##How to run?
+In order to run the script, you would require the following R packages
+1.R base package
+2.dplyr package
+
+You will need to put the UCI HAR Dataset downloaded through here: 
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+Extract and put all the files (including all sub-directories) to your R working directory
+
+##Inputs
+UCI HAR Dataset downloaded through this link
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+##Outputs
+data3 - a merged data frame from the training set and test set data, alongside with subjects and activities.
+clean_dataset - an independent data set containing the mean values of each feature variable for each subject for each activity 
--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,7 @@
 ##Load in the file and merge into one dataset
-testset <- read.table("C:/Users/User/Desktop/UCI HAR Dataset/test/X_test.txt")
-trainset <- read.table("C:/Users/User/Desktop/UCI HAR Dataset/train/X_train.txt")
+testset <- read.table("~/UCI HAR Dataset/test/X_test.txt")
+trainset <- read.table("~/UCI HAR Dataset/train/X_train.txt")
+
 
 data <- rbind(testset,trainset) #Merge to form one big dataset
 
@@ -15,7 +16,10 @@ data2 <- data[,c(mean_idx,std_idx)]
 act_train <- read.table("C:/Users/User/Desktop/UCI HAR Dataset/train/y_train.txt", col.names="Activity")
 act_test <- read.table("C:/Users/User/Desktop/UCI HAR Dataset/test/y_test.txt",col.names="Activity")
 
-data3 <- cbind(data2,rbind(act_test, act_train)) ##Merge Activity column with the processed data2 
+testsub <- read.table("~/UCI HAR Dataset/test/subject_test.txt", col.names="Subject")
+trainsub <- read.table("~/UCI HAR Dataset/train/subject_train.txt", col.names="Subject")
+
+data3 <- cbind(data2,rbind(testsub, trainsub), rbind(act_test, act_train)) ##Merge Activity column with the processed data2 
 
 ##Label the activity names
 data3$Activity[data3$Activity == 1] <- "Walking"
@@ -26,4 +30,39 @@ data3$Activity[data3$Activity == 5] <- "Standing"
 data3$Activity[data3$Activity == 6] <- "Laying"
 
 ##Name the mean and std features
+#Set up the mapping table
+fullname <- data.frame("OriginalName" = c("tBodyAcc","tGravityAcc","tBodyAccJerk", "tBodyGyro", "tBodyGyroJerk", "tBodyAccMag", "tGravityAccMag", "tBodyAccJerkMag", "tBodyGyroMag", "tBodyGyroJerkMag", "fBodyAcc", "fBodyAccJerk", "fBodyGyro", "fBodyAccMag", "fBodyAccJerkMag", "fBodyGyroMag", "fBodyGyroJerkMag","fBodyBodyAccJerkMag", "fBodyBodyGyroMag", "fBodyBodyGyroJerkMag"),   "FullName" = c("Body Acceleration Signal in time", "Gravity Acceleration signal in time", "Body Acceleration Jerk Signal in time", "Body Gyroscope signal in time", "Body Gyroscope jerk signal in time", "Body Acceleration signal magnitude in time", "Gravity Acceleration signal magnitude in time", "Body Accerlation Jerk signal magnitude in time", "Body Gyroscope signal magnitude in time", "Body Gyroscope jerk signal in time", "Body Acceration signal in frequency" ,"Body Acceleration Jerk signal in frequency", "Body Gyroscope signal in frequency", "Body Acceleration signal magnitude in frequency", "Body Acceleration Jerk signal magnitude in frequency", "Body Gyroscope signal magnitude in frequency", "Body Gyroscope jerk signal magnitude in frequency", "Body Body Acceleration jerk signal magnitude in frequency", "Body Body gyroscope signal magnitude in frequency", "Body Body gyroscope jerk signal in magnitude")) ##Define Full name mapping table
+statname <- data.frame("OriginalName" = c("mean()","std()", "meanFreq()"), "FullName" = c("mean", "standard deviation", "weight average frequency")) ##stat full name
+direction <- data.frame("OriginalName" = c("X","Y", "Z"), "FullName" = c("X direction", "Y direction", "Z direction"))
+
+#Extract the features
 new_features <- features[c(mean_idx,std_idx),]
+
+#Fill the full names
+splitstr <- strsplit(new_features$Name, "-")
+
+for (i in 1:length(new_features$Name))
+{
+        vName <- as.character(fullname$FullName[fullname$OriginalName==splitstr[[i]][1]])
+        sName <- as.character(statname$FullName[statname$OriginalName==splitstr[[i]][2]])
+        
+        new_features$FullName[i] <- paste(sName, "of", vName)
+        
+        if(is.na(splitstr[[i]][3]) == FALSE)
+        {
+                dName <- as.character(direction$FullName[direction$OriginalName==splitstr[[i]][3]])
+                new_features$FullName[i] <- paste(new_features$FullName[i], "in", dName)
+        }
+        
+}
+
+#Set the feature names!
+names(data3) <- c(new_features$FullName,"Subject", "Activity")
+
+#Get a new dataset with the mean of each activity of each subject
+library(dplyr)
+clean_dataset <- tbl_df(data3) %>% group_by(Activity, Subject)
+clean_dataset <- clean_dataset %>% summarise_each(funs(mean))
+
+#Output the file
+write.table(clean_dataset, file="step5.txt", row.names=FALSE)
